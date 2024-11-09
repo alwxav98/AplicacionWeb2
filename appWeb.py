e58f466f728d68e3b4175d4343551930c3fac7e4
alwxav98 <107285163+alwxav98@users.noreply.github.com>
2024-11-09 01:13:11 -0500

    Docker compose file implementation

--- appWeb.py
+++ appWeb.py
@@ -7,4 +7,4 @@ def home():
     return render_template('index.html', ViewData={"Title": "Aplicacion Web 2"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
