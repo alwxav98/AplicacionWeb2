9b4ae8899019274cfb56f27749efbba9f568829b
alwxav98 <107285163+alwxav98@users.noreply.github.com>
2024-11-09 00:40:58 -0500

    Template added

--- appWeb.py
+++ appWeb.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "¡Hola, mundo!"
+    return render_template('index.html', ViewData={"Title": "Aplicacion Web 2"})
 
 if __name__ == "__main__":
     app.run(debug=True)
